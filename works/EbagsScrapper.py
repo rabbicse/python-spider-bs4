53ec57dc27a4b98c81dab25aa2458caf0de75730
Rabbi <rabbi.se@gmail.com>
2014-02-10 00:17:18 +0600

    Modified Ebags spider.

--- works/EbagsScrapper.py
+++ works/EbagsScrapper.py
@@ -1,3 +1,4 @@
+import os
 import re
 import time
 from logs.LogManager import LogManager
@@ -22,6 +23,8 @@ class EbagsScrapper():
     def scrapData(self):
         data = self.spider.fetchData(self.url)
         if data and len(data) > 0:
+            data = self.regex.reduceNewLine(data)
+            data = self.regex.reduceBlankSpace(data)
             allBrandLinks = []
             soup = BeautifulSoup(data)
             brandChunks = soup.find_all('div', class_='brandList4Col')
@@ -31,24 +34,47 @@ class EbagsScrapper():
                     if brandLinks and len(brandLinks) > 0:
                         for brandLink in brandLinks:
                             brandUrl = brandLink.get('href')
-                            print brandUrl
                             allBrandLinks.append(self.mainUrl + brandUrl)
 
-            print allBrandLinks
+            print 'Total brands found: ', str(len(allBrandLinks))
             for brandUrl in allBrandLinks:
                 self.scrapBrandData(brandUrl)
-                break
 
-    def scrapBrandData(self, url):
-        print 'Brand URL: ' + url
-        data = self.spider.fetchData(url)
+    def scrapBrandData(self, url, page=1):
+        brandUrl = url if page is 1 else url + '?page=' + str(page)
+        print 'Brand URL: ', brandUrl
+        data = self.spider.fetchData(brandUrl)
         if data and len(data) > 0:
+            data = self.regex.reduceNewLine(data)
+            data = self.regex.reduceBlankSpace(data)
             soup = BeautifulSoup(data)
             productLinks = soup.find_all('div', class_='itemProductName')
             if productLinks and len(productLinks) > 0:
                 for productLink in productLinks:
                     productUrl = productLink.find('a').get('href')
-                    print self.mainUrl + productUrl
+                    productDetailUrl = self.mainUrl + productUrl
+                    print 'Product detail url: ', productDetailUrl
+                    fileName = self.regex.getSearchedData('(?i)^.*?(\d+)$', productUrl) + '.html'
+                    print 'File Name: ', fileName
+                    self.scrapProductDetail(self.mainUrl + productUrl, fileName)
 
-    def scrapProductDetail(self, url):
+            ## Check if any next page
+            if soup.find('a', class_='pagingNext') is not None:
+                nextLink = soup.find('a', class_='pagingNext').get('href')
+                print nextLink
+                if nextLink is not None and len(nextLink) > 0:
+                    print 'Pagination found...'
+                    return self.scrapBrandData(url, page + 1)
+
+    def scrapProductDetail(self, url, filename):
         data = self.spider.fetchData(url)
+        if data and len(data) > 0:
+            data = self.regex.reduceNewLine(data)
+            data = self.regex.reduceBlankSpace(data)
+            directory = os.path.dirname('./products/' + filename)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            print 'File saving path: ', os.path.abspath('./products/' + filename)
+            dl_file = open('./products/' + filename, 'wb')
+            dl_file.write(data)
+            dl_file.close()
